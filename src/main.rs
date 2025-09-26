e8f9a9627aed7c59fb967c03b6693f9e476e7ec5
Leonardo Tonezi <leoademir0@gmail.com>
2025-09-26 00:03:27 -0300

    feat: add simple lose functionality

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::error::Error;
+use std::{error::Error, time::Duration};
 
+use crossterm::{cursor::{Hide, Show}, event::{self, Event, KeyCode}, terminal::{self, EnterAlternateScreen, LeaveAlternateScreen}, ExecutableCommand};
 use rusty_audio::Audio;
+use std::io;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -12,7 +14,34 @@ fn main() -> Result<(), Box<dyn Error>> {
     audio.add("win", "assets/audio/win.wav");
     audio.play("startup");
 
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+
+    // Game Loop
+    'gameloop: loop {
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+    
+
     //Cleanup
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode();
     Ok(())
 }
