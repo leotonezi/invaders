14233f271d3be70b1b26c483034ebaa197639d92
Leonardo Tonezi <leoademir0@gmail.com>
2025-10-01 22:32:38 -0300

    feat: rendering and multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::{error::Error, time::Duration};
+use std::{error::Error, sync::mpsc, thread, time::Duration};
 
 use crossterm::{cursor::{Hide, Show}, event::{self, Event, KeyCode}, terminal::{self, EnterAlternateScreen, LeaveAlternateScreen}, ExecutableCommand};
 use rusty_audio::Audio;
 use std::io;
+use invaders::{frame, render};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -20,9 +21,26 @@ fn main() -> Result<(), Box<dyn Error>> {
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
 
+    // Render Loop in a separate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = invaders::frame::new_frame();
+        let mut stdout = io::stdout();
+        invaders::render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            invaders::render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
 
     // Game Loop
     'gameloop: loop {
+        // Per Frame Init
+        let curr_frame = frame::new_frame();
         // Input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
@@ -35,13 +53,18 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
             }
         }
+        // Draw and render
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
     }
     
 
     //Cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
     audio.wait();
     stdout.execute(Show)?;
     stdout.execute(LeaveAlternateScreen)?;
-    terminal::disable_raw_mode();
+    terminal::disable_raw_mode()?;
     Ok(())
 }
